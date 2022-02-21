794f27af3daaf3ef8b6e8f3ea09297617ac49104
brandib222 <brandi.danielle.ball@gmail.com>
2022-02-21 16:00:42 -0500

    finished the auth middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,6 @@
+//const req = require('express/lib/request');
+const Users = require('../users/users-model');
+
 /*
   If the user does not have a session saved in the server
 
@@ -6,8 +9,12 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  if(req.session.user == null) {
+      next({ status: 401, message: 'You shall not pass!' });
+  } else {
+      next();
+  }
 }
 
 /*
@@ -18,8 +25,12 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+function checkUsernameFree(req, res, next) {
+    if(Users.findBy({ username: req.user.username }).first() != null) {
+      next({ status: 422, message: `Username taken` });
+  } else {
+      next();
+}
 }
 
 /*
@@ -30,8 +41,14 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+async function checkUsernameExists(req, res, next) {
+  const user = await Users.findBy({ username: req.user.username }).first();
+    if(user == null) {
+        next({ status: 401, message: `Invalid credentials` });
+    } else {
+        req.user = user;
+        next();
+    }
 }
 
 /*
@@ -42,8 +59,18 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+function checkPasswordLength(req, res, next) {
+  if(!req.body.password || req.body.password.length < 3) {
+    next({ status: 422, message: 'Password must be longer than 3 chars'})
+  } else {
+    next();
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = { 
+  restricted, 
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
+}
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -28,16 +28,16 @@ function findBy(filter) {
  */
 function findById(user_id) {
   return db('users')
-  .where({ id })
+  .where({ user_id })
   .first()
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
-function add(user) {
+async function add(user) {
   const [id] = await db('users').insert(user)
-
+  return findById(id)
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
